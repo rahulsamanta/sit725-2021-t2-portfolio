6bf6f84312afe39ebb4239354760c3cbc67938aa
Rahul Samanta <samanta.rahul@outlook.com>
2021-08-10 20:27:33 +1000

    Added database connection and testing connectivity with dummy record insertion.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# sit725-2021-t2-prac2
+# sit725-2021-t2-prac4
 Tasks related to SIT725 Practicals
+
+Update: Added database connectivity
 
 This is a simple express server demonstrating endpoints and serving static HTML pages with Materialize CSS.
 This template is very basic and will ultimately build up to my Portfolio page as part of the SIT725 practicals.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { MongoClient } = require('mongodb');
 
 const app = express()
 const PORT=3000;
@@ -18,3 +19,42 @@ app.get('/test',function(req, res){
 // start the server and listen on port 3000
 app.listen(PORT)
 console.log('Server started at http://localhost:' + PORT);
+
+// Function to connect to MongoDB and perform operations
+async function database(){
+    
+    const uri = "mongodb+srv://admin:SVJ2iux5XUZG7Xak@sit725-cluster.lm2gt.mongodb.net/portfolio?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        console.log("Database connection successful!");
+
+        // Insert a single record into database collection
+        await insertRecord(client,
+            {
+                name: "Rahul Samanta",
+                description: "Software Developer",
+                projects: 3
+            }
+        );
+
+        console.log("Record insertion completed!");
+        
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+database().catch(console.error);
+
+// Inserting single entry into database collection
+async function insertRecord(client, newRecord){
+    const result = await client.db("portfolio").collection("selfDetails").insertOne(newRecord);
+    console.log(`New record created with the following id: ${result.insertedId}`);
+}
